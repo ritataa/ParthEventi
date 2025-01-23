4494709b7d2535f6002dc1542141e8962f59ba5b
rita <rita.tammaro@icloud.com>
2025-01-23 17:50:47 +0100

    Aggiunta la tabella utenti_registrati per gestire le informazioni degli utenti registrati

---
+++ db/login.sql
@@ -0,0 +1,8 @@
+CREATE TABLE utenti_registrati (
+    id_utente INT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    cognome VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    data_registrazione DATE DEFAULT CURRENT_DATE
+);

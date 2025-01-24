e798ce513d90f040c39603a0eb2b624ab0142d8a
rita <rita.tammaro@icloud.com>
2025-01-24 22:48:29 +0100

    Aggiunta la logica per avviare il client in base all'opzione selezionata; aggiornato il caricamento della configurazione del server.

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 # Contenuto di main.py
 
 import json
-from serverE import start_server
+import os
 
 def load_server_config():
     with open('server_address.json') as config_file:
         return json.load(config_file)
 
+def start_client(client_file):
+    os.system(f"python {client_file}")
+
 if __name__ == "__main__":
     config = load_server_config()
-    address = config['server']['address']
-    port = config['server']['port']
+    address = config['address']
+    port = config['port']
     
-    start_server(address, port)
+    print("Seleziona un'opzione:")
+    print("1. Registrazione Partecipante")
+    print("2. Verifica Validità Tessere")
+    print("3. Sospensione/Attivazione Accesso")
+    
+    choice = input("Inserisci il numero dell'opzione: ")
+    
+    if choice == "1":
+        start_client("client/client.py")
+    elif choice == "2":
+        start_client("client/clientC.py")
+    elif choice == "3":
+        start_client("client/clientO.py")
+    else:
+        print("Opzione non valida")
